2f50ba5e83c13e7b7c16765dde5427dc59d677e0
DanBed666 <daniel.bednarczyk@outlook.com>
2022-10-25 16:12:52 +0200

    poprawa kodu z lab2 zgodnie ze standardem pep8

--- zad2A.py
+++ zad2A.py
@@ -2,6 +2,7 @@ def zad2a(lst):
     for e in lst:
         print(e)
 
+
 if __name__ == '__main__':
 
     zad2a(["Damian", "Mateusz", "Krzysztof", "Grzegorz", "Marian"])
--- zad2D.py
+++ zad2D.py
@@ -2,6 +2,7 @@ def zad2d(lst):
 
     return [i for i in lst[::2]]
 
+
 if __name__ == '__main__':
 
     print(zad2d([i for i in range(1, 11)]))
