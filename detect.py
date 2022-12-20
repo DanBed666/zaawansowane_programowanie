f47a449a51ef6cb6b5d92d922d0d28f6097634cd
DanBed666 <daniel.bednarczyk@outlook.com>
2022-12-20 14:32:40 +0100

    ulepszono wykrywanie i dodano frontend

---
+++ detect.py
@@ -0,0 +1,50 @@
+import base64
+
+import cv2 as cv
+from PIL import Image
+from matplotlib import cm
+import numpy as np
+from io import BytesIO
+
+def pict(name):
+
+    countx = []
+
+    cvNet = cv.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'graph.pbtxt')
+
+    img = cv.imread(name)
+    rows = img.shape[0]
+    cols = img.shape[1]
+    cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
+    cvOut = cvNet.forward()
+
+    for detection in cvOut[0, 0, :, :]:
+        score = float(detection[2])
+        # ustawienie odpowiedniej wagi
+        if score > 0.6:
+            left = detection[3] * cols
+            top = detection[4] * rows
+            right = detection[5] * cols
+            bottom = detection[6] * rows
+            countx.append(score)
+            cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 100, 75), thickness=4)
+            print(score)
+
+    print(type(img))
+    im = Image.fromarray(img)
+    im.thumbnail((800, 800), Image.ANTIALIAS)
+    buffered = BytesIO()
+    im.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    print(type(im))
+    print(type(img_str))
+
+    return len(countx), img_str
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,49 @@
+from PIL import Image
+from fastapi import FastAPI, Request, Form, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import shutil
+import detect
+import base64
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/result", response_class=HTMLResponse)
+async def root(request: Request, file: UploadFile = File(...)):
+
+    try:
+        with open(f'{file.filename}', "wb") as buffer:
+            buffer.write(file.file.read())
+            shutil.copyfileobj(file.file, buffer)
+
+        people, img = detect.pict(buffer.name)
+
+        return templates.TemplateResponse("result.html", {"request": request, "people": people, "myImage": img})
+    except (AttributeError, FileNotFoundError):
+        return templates.TemplateResponse("index.html", {"request": request, "message": "Niepoprawny format pliku!"})
+
+
+@app.get("/result", response_class=HTMLResponse)
+async def root(request: Request, file: UploadFile = File(...)):
+
+    try:
+        with open(f'{file.filename}', "wb") as buffer:
+            buffer.write(file.file.read())
+            shutil.copyfileobj(file.file, buffer)
+
+        people, img = detect.pict(buffer.name)
+
+        return templates.TemplateResponse("result.html", {"request": request, "people": people, "myImage": img})
+    except (AttributeError, FileNotFoundError):
+        return templates.TemplateResponse("index.html", {"request": request, "message": "Niepoprawny format pliku!"})
+
+
+
+
